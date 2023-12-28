a80980115b9c88662bbeb040a7f06a63d28dbaf2
bozhaohui <buzhaohui@zzljy.onaliyun.com>
2023-12-28 17:34:59 +0800

    fix

--- src/ios/CtyNoticePlugin.m
+++ src/ios/CtyNoticePlugin.m
@@ -12,6 +12,8 @@
      -(void) userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler;
      -(void)timedCancelNotice:(CDVInvokedUrlCommand*)command;
      -(void)bigImageNotice:(CDVInvokedUrlCommand*)command;
+     -(void)largeTextNotice:(CDVInvokedUrlCommand*)command;
+     -(void)importantNotice:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation CtyNoticePlugin
@@ -68,7 +70,7 @@
     }];
 }
 
-- (void)bigImageNotice:(CDVInvokedUrlCommand *)command{
+ -(void)largeTextNotice:(CDVInvokedUrlCommand*)command {
     
     NSArray* arguments = command.arguments;
     NSString* notificationId = [arguments objectAtIndex:0];
@@ -96,16 +98,133 @@
     //设置从通知激活App时的lanunchImage图片
     //content.lauchImageName = @"lanunchImage";
 
-    UNNotificationAttachment *attachment=[UNNotificationAttachment attachmentWithIdentifier:@"imageAttachment" URL:[NSURL URLWithString:urlBigImage] options:nil error:nil];
-    
-    content.attachments=@[attachment];
+    //通知触发时间
+    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:0.1 repeats:NO];
 
+    //设置通知请求
+    //如果使用相同的[requestWithIdentifier]会一直覆盖之前的旧通知
+    NSString* identifier = [NSUUID UUID].UUIDString;
+
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+
+    //将通知添加到UNUserNotificationCenter中
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+    if (error) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+     } else {
+        [center addNotificationRequest:request withCompletionHandler:nil];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"success"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+     }
+    }];
+}
+
+- (void)importantNotice:(CDVInvokedUrlCommand *)command {
+    
+    NSArray* arguments = command.arguments;
+    NSString* notificationId = [arguments objectAtIndex:0];
+    NSString* title = [arguments objectAtIndex:1];
+    NSString* subtitle = [arguments objectAtIndex:2];
+    NSString* message = [arguments objectAtIndex:3];
+    NSString* urlLargeIco = [arguments objectAtIndex:4];
+    NSString* urlBigImage = [arguments objectAtIndex:5];
+    NSString* strDate = [arguments objectAtIndex:6];
+    NSString* strRepeat = [arguments objectAtIndex:7];
+    NSString* strType = [arguments objectAtIndex:8];
+    
+    //通知内容
+    UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+    //设置通知请求发送时APP图标上显示的数字
+    content.badge=@0;
+    //通知标题
+    content.title=title;
+    //通知副标题
+    content.subtitle=subtitle;
+    //通知内容
+    content.body=message;
+    //通知声音
+    content.sound=[UNNotificationSound defaultSound];
+    //设置从通知激活App时的lanunchImage图片
+    //content.lauchImageName = @"lanunchImage";
+
+    //通知触发时间
+    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:0.1 repeats:NO];
+
+    //设置通知请求
+    //如果使用相同的[requestWithIdentifier]会一直覆盖之前的旧通知
+    NSString* identifier = [NSUUID UUID].UUIDString;
+
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+
+    //将通知添加到UNUserNotificationCenter中
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+    if (error) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+     } else {
+        [center addNotificationRequest:request withCompletionHandler:nil];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"success"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+     }
+    }];
+}
+
+
+
+- (void)bigImageNotice:(CDVInvokedUrlCommand *)command{
+    
+    NSArray* arguments = command.arguments;
+    NSString* notificationId = [arguments objectAtIndex:0];
+    NSString* title = [arguments objectAtIndex:1];
+    NSString* subtitle = [arguments objectAtIndex:2];
+    NSString* message = [arguments objectAtIndex:3];
+    NSString* urlLargeIco = [arguments objectAtIndex:4];
+    NSString* urlBigImage = [arguments objectAtIndex:5];
+    NSString* strDate = [arguments objectAtIndex:6];
+    NSString* strRepeat = [arguments objectAtIndex:7];
+    NSString* strType = [arguments objectAtIndex:8];
+    
+    //通知内容
+    UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+    //设置通知请求发送时APP图标上显示的数字
+    content.badge=@0;
+    //通知标题
+    content.title=title;
+    //通知副标题
+    content.subtitle=subtitle;
+    //通知内容
+    content.body=message;
+    //通知声音
+    content.sound=[UNNotificationSound defaultSound];
+    //设置从通知激活App时的lanunchImage图片
+    //content.lauchImageName = @"lanunchImage";
+   
+    NSURL *imageURL=[NSURL URLWithString:urlBigImage];
+    
+    NSData *imageData=[NSData dataWithContentsOfURL:imageURL];
+    
+    // Save the downloaded image to a temporary file
+    NSString *temporaryDirectory = NSTemporaryDirectory();
+    NSString *imagePath = [temporaryDirectory stringByAppendingPathComponent:@"image.jpg"];
+    [imageData writeToFile:imagePath atomically:YES];
+    
+    // Create a UNNotificationAttachment with the temporary image file
+    UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"imageAttachment" URL:[NSURL fileURLWithPath:imagePath] options:nil error:nil];
+    if (attachment) {
+        // Attach the image to the notification content
+        content.attachments = @[attachment];
+    }
     //通知触发时间
     UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:0.1 repeats:NO];
     
     NSString* identifier = [NSUUID UUID].UUIDString;
 
-    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger,options:nil];
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
     
     //将通知添加到UNUserNotificationCenter中
     UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
@@ -158,10 +277,25 @@
     NSCalendar *calendar = [NSCalendar currentCalendar];
     NSDateComponents *dateComponents = [calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond fromDate:nsDate];
 
-
-    
     BOOL* repeats =false;
-
+    
+    
+    NSURL *imageURL=[NSURL URLWithString:urlBigImage];
+    
+    NSData *imageData=[NSData dataWithContentsOfURL:imageURL];
+    
+    // Save the downloaded image to a temporary file
+    NSString *temporaryDirectory = NSTemporaryDirectory();
+    NSString *imagePath = [temporaryDirectory stringByAppendingPathComponent:@"image.jpg"];
+    [imageData writeToFile:imagePath atomically:YES];
+    
+    // Create a UNNotificationAttachment with the temporary image file
+    UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"imageAttachment" URL:[NSURL fileURLWithPath:imagePath] options:nil error:nil];
+    if (attachment) {
+        // Attach the image to the notification content
+        content.attachments = @[attachment];
+    }
+    
     UNCalendarNotificationTrigger *trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:dateComponents repeats:repeats];
 
     //设置通知请求
