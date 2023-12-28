a41fe0f034702c6cd6a63597ee82a7461b019650
bozhaohui <buzhaohui@zzljy.onaliyun.com>
2023-12-28 14:39:39 +0800

    fix

--- src/ios/CtyNoticePlugin.m
+++ src/ios/CtyNoticePlugin.m
@@ -1,14 +1,17 @@
 #import <cordova/CDV.h>
 #import <Foundation/Foundation.h>
 #import <CommonCrypto/CommonCrypto.h>
+#import <UserNotifications/UserNotifications.h>
 
-@interface CtyNoticePlugin : CDVPlugin
-     CDVPluginResult* pluginResult;
+@interface CtyNoticePlugin : CDVPlugin{
+    CDVPluginResult* pluginResult;
+}
      -(void) commonNotice:(CDVInvokedUrlCommand*)command;
      -(void) timedNotice:(CDVInvokedUrlCommand*)command;
      -(void) userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler;
      -(void) userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler;
-    - (void)timedCancelNotice:(CDVInvokedUrlCommand*)command;
+     -(void)timedCancelNotice:(CDVInvokedUrlCommand*)command;
+     -(void)BigPicturenotice:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation CtyNoticePlugin
@@ -58,20 +61,69 @@
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
      } else {
-        // Move the creation of content and trigger here
-        UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
-        // Set content properties
-        // ...
-        UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:0.1 repeats:NO];
-        // ...
-        // Add the notification request
         [center addNotificationRequest:request withCompletionHandler:nil];
-
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"success"];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
      }
-}]
+    }];
 }
+
+- (void)BigPicturenotice:(CDVInvokedUrlCommand *)command{
+    
+    NSArray* arguments = command.arguments;
+    NSString* notificationId = [arguments objectAtIndex:0];
+    NSString* title = [arguments objectAtIndex:1];
+    NSString* subtitle = [arguments objectAtIndex:2];
+    NSString* message = [arguments objectAtIndex:3];
+    NSString* urlLargeIco = [arguments objectAtIndex:4];
+    NSString* urlBigImage = [arguments objectAtIndex:5];
+    NSString* strDate = [arguments objectAtIndex:6];
+    NSString* strRepeat = [arguments objectAtIndex:7];
+    NSString* strType = [arguments objectAtIndex:8];
+    
+    //通知内容
+    UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+    //设置通知请求发送时APP图标上显示的数字
+    content.badge=@0;
+    //通知标题
+    content.title=title;
+    //通知副标题
+    content.subtitle=subtitle;
+    //通知内容
+    content.body=message;
+    //通知声音
+    content.sound=[UNNotificationSound defaultSound];
+    //设置从通知激活App时的lanunchImage图片
+    //content.lauchImageName = @"lanunchImage";
+
+    //通知触发时间
+    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:0.1 repeats:NO];
+
+    //设置通知请求
+    //如果使用相同的[requestWithIdentifier]会一直覆盖之前的旧通知
+    NSString* identifier = [NSUUID UUID].UUIDString;
+
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+
+    UNNotificationAttachment *attachment=[UNNotificationAttachment attachmentWithIdentifier:@"image" URL:urlBigImage options:nil error:nil];
+    
+    content.attachments=@[attachment];
+    //将通知添加到UNUserNotificationCenter中
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+    if (error) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+     } else {
+         
+        [center addNotificationRequest:request withCompletionHandler:nil];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"success"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+     }
+    }];
+}
+
 //定时通知
 -(void)timedNotice:(CDVInvokedUrlCommand*)command {
     
@@ -94,7 +146,7 @@
     //通知副标题
     content.subtitle=subtitle;
     //通知内容
-    content.body=body;
+    content.body=message;
     //通知声音
     content.sound=[UNNotificationSound defaultSound];
     //设置从通知激活App时的lanunchImage图片
@@ -111,7 +163,7 @@
 
 
     
-    BOOL* repeats = [bRepeats boolValue];
+    BOOL* repeats =false;
 
     UNCalendarNotificationTrigger *trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:dateComponents repeats:repeats];
 
@@ -127,7 +179,10 @@
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
 
         }
-    }]
+    }];
+    
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"success"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 
@@ -173,7 +228,7 @@
         if (error) {
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
         }
-    }]
+    }];
 }
 
 
@@ -187,4 +242,4 @@
     completionHandler();
 }
 
-
+@end
